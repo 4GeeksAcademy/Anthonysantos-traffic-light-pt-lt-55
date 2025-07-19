da323dba00f064c8c9b90cad1798f3d5e068c489
Anthony González <asgtoribio@gmail.com>
2025-07-19 01:11:09 +0000

    corregido el css brightRed...Fin

--- src/js/components/TrafficLight.jsx
+++ src/js/components/TrafficLight.jsx
@@ -5,7 +5,7 @@ const TrafficLight = () => {
   const [red, setRed] = useState('red');
   const [yellow, setYellow] = useState('yellow');
   const [green, setGreen] = useState('green');
-  
+
   const [redBright, setRedBright] = useState(false);
   const [yellowBright, setYellowBright] = useState(false);
   const [greenBright, setGreenBright] = useState(false);
@@ -31,7 +31,7 @@ const TrafficLight = () => {
     <div className="d-flex justify-content-center align-items-center min-vh-100">
         <div className="position-absolute top-0 bg-black rounded" style={{ width: '20px', height: '20%' }}> </div>
         <div className="d-flex flex-column align-items-center justify-content-center bg-black p-4" style={{ width: '15%' }}>
-          <div onClick={changeRed} className={redBright ? 'bright' : ''} style={{backgroundColor: red, width: '100px', height: '100px', borderRadius: '50%', border: 'none', marginBottom: '20px',marginTop:'10px'}}>
+          <div onClick={changeRed} className={redBright ? 'brightRed' : ''} style={{backgroundColor: red, width: '100px', height: '100px', borderRadius: '50%', border: 'none', marginBottom: '20px',marginTop:'10px'}}>
           </div>
         
         <div onClick={changeYellow} className={yellowBright ? 'brightYellow' : ''} style={{backgroundColor: yellow, width: '100px', height: '100px', borderRadius: '50%', border: 'none', marginBottom: '20px'}}>
